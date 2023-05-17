4d0f856a079f7943402627986e254bb2ec8bbf77
shirinrz <shirinrazi68@gmail.com>
2023-05-17 14:28:19 -0700

    change icon color

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="#9bdeec"
+      color="azure"
       size={120}
       animate={true}
     />
